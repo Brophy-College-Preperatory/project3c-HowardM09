2162bf6f6f7a188ba3f942bbd384e530f6a269fd
HowardM09 <hmullins28@brophybroncos.org>
2025-03-21 20:33:19 +0000

    change

--- change.c
+++ change.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void){
-    //Work here
-    printf("hello");
+    int x;
 
+    printf("Hello. How much change is owed? Enter as a whole number Example: 0.41 as 41\n");
+    scanf("%d", &x);
 
+    printf("Quarters: %d\n", x/25);
+    x = x % 25;
+    printf("Dimes: %d\n", x/10);
+    x = x % 10;
+    printf("Nickels: %d\n", x/5);
+    x = x % 5;
+    printf("Pennies: %d\n", x);
+ 
 
+    printf("Would you like to make change for another amount? Enter 1 for yes and 0 for no\n");
+    scanf("%d", &x);
+    if(x == 1){
+        main();
+    }
+    else{
+        printf("Goodbye\n");
+    }
     return 0;
 }
